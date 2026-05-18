34035f02a04c83f3a68008a324c3681b8fd96146
Leslie <lesmwendwa@gmail.com>
2026-05-18 13:00:11 +0300

    added base and home templates

--- restaurant/restaurant/settings.py
+++ restaurant/restaurant/settings.py
@@ -73,12 +73,23 @@ WSGI_APPLICATION = 'restaurant.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+}'''
+
+DATABASES = {
+     'default': {
+         'ENGINE': 'django.db.backends.postgresql',
+         'NAME': config('DB_NAME'),
+         'USER': config('DB_USER'),
+         'PASSWORD': config('DB_PASSWORD'),
+         'HOST': config('DB_HOST'),
+         'PORT': config('DB_PORT'),
+     }
+ }
 
 
 # Password validation
