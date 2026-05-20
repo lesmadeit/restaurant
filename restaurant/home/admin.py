477fdfed32b4cbbf9b91d0b02e88f128d8f2d9a4
Leslie <lesmwendwa@gmail.com>
2026-05-20 12:30:36 +0300

    menu backend models and admin

--- restaurant/home/admin.py
+++ restaurant/home/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import MenuCategory, MenuItem
 
-# Register your models here.
+
+@admin.register(MenuCategory)
+class MenuCategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'order']
+    prepropulated_fields = {'slug': ('name',)}
+
+
+@admin.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description']
+    list_filter = ['category', 'is_available']
+    search_fields = ['name', 'description']
+    ordering = ['category', 'name']
--- restaurant/home/models.py
+++ restaurant/home/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+
+class MenuCategory(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True, blank=True)
+    order = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        ordering = ['order', 'name']
+        verbose_name_plural = "Menu Categories"
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+    
+class MenuItem(models.Model):
+    category = models.ForeignKey(
+        MenuCategory,
+        on_delete=models.CASCADE,
+        related_name='items'
+    )
+
+    name = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to='menu/', blank=True, null=True)
+    is_available = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['category', 'name']
+
+    def __str__(self):
+        return f"{self.name} - ${self.price}"
+
+
