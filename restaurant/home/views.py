a04594c1caf4ce5260a0d2baeccd2f08d01636a8
Leslie <lesmwendwa@gmail.com>
2026-05-21 11:42:14 +0300

    changes

--- restaurant/home/views.py
+++ restaurant/home/views.py
@@ -2,15 +2,13 @@ from django.shortcuts import render
 from .models import MenuCategory, MenuItem
 
 def home(request):
-    return render(request, 'home/home.html')
-
-def menu_view(request):
     categories = MenuCategory.objects.all()
     menu_items = MenuItem.objects.filter(is_available=True).select_related('category')
-
 
     context = {
         'categories': categories,
         'menu_items': menu_items,
     }
-    return render(request, 'menu.html', context)
+    return render(request, 'home/home.html', context)
+
+
